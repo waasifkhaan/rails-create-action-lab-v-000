a668becafab54a34cebe34d312082821654912be
Wasif Khan <waasif.khaan@gmail.com>
2018-09-29 14:47:46 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,7 +15,8 @@ class StudentsController < ApplicationController
     @student = Student.new 
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
-    @student.save
+    @student.save 
+    redirect_to student_path(@student)
   end
 
 end
