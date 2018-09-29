8d7add6fdfbb46471fb2d0b2b0d0050c7360075e
Wasif Khan <waasif.khaan@gmail.com>
2018-09-29 14:47:28 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,7 +14,7 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new 
     @student.first_name = params[:first_name]
-    @student.last_name
+    @student.last_name = params[:last_name]
   end
 
 end
